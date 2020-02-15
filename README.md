337d876301fe49fccf406e9f7a5f8fb26f8f97ec
Tim <timcearl@gmail.com>
2020-02-15 16:47:58 +0000

    Setup repo

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sicp
+
+Just me doing the SICP examples.
+
+https://mitpress.mit.edu/sites/default/files/sicp/index.html
+
+## Usage
+
+Run stuff in the REPL
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject sicp "0.1.0-SNAPSHOT"
+  :description "Structure & Interpretation of Computer Programs - Exercises"
+  :url "https://mitpress.mit.edu/sites/default/files/sicp/index.html"
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :profiles {:uberjar {:aot :all}})
